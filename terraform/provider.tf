9dcabf58eb34f86f50ed21fc1fff8f59eba7faf1
Tomas Kubica <tkubica12@gmail.com>
2022-08-19 06:49:45 +0200

    move state to different subscription

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,10 +10,11 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "shared-services"
-    storage_account_name = "tomasiac"
+    resource_group_name  = "base"
+    storage_account_name = "tkubicastore"
     container_name       = "tfstate"
     key                  = "landingzone.tfstate"
+    subscription_id      = "d3b7888f-c26e-4961-a976-ff9d5b31dfd3"
   }
 }
 
